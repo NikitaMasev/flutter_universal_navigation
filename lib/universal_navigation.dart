3477cf24c87d7a7c61573c31340caa0f17744fec
Nikita Masev <Nikita.Masev@gmail.com>
2020-11-19 15:25:45 +0300

    export all imports of library into universal_navigation.dart; fix example project; fix web support;

--- lib/universal_navigation.dart
+++ lib/universal_navigation.dart
@@ -2,24 +2,15 @@ library universal_navigation;
 
 export 'event_notifier/core_event_notifier.dart';
 
-//export 'navigation/core/app_navigator.dart';
 export 'navigation/core/bottom_navigation_builder.dart';
 export 'navigation/core/navigation_controller.dart';
-//export 'navigation/core/navigation_controller_events.dart';
-//export 'navigation/core/navigation_events.dart';
 export 'navigation/core/tab_change_listener.dart';
-
-//export 'navigation/models/navigation_data/base_navigation_arguments.dart';
-//export 'navigation/models/navigation_data/navigation_arguments.dart';
-//export 'navigation/models/navigation_data/navigation_tab_arguments.dart';
 
 export 'navigation/models/navigation_flow_data/globalflows.dart';
 export 'navigation/models/navigation_flow_data/tabflow.dart';
 
 export 'navigation/models/navigation_keys/bottom_nav_key.dart';
 export 'navigation/models/navigation_keys/global_nav_key.dart';
-
-//export 'navigation/models/navigation_type_events.dart';
 
 export 'navigation/bottom_navigation_page.dart';
 
